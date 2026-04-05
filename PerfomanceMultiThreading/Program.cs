a6381154a849aa75130e00f27fbe65b01a3b666e
mathewsun <mathew.dubrovin@gmail.com>
2026-04-05 21:10:14 +0300

    test perf for mt

---
+++ PerfomanceMultiThreading/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace PerfomanceMultiThreading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] threadCounts = { 1, 2, 4, 8, 12, 18, 24, 40, 50, 100, 200, 500, 1000, 5000, 10000, 50000, 100000 };
+            long workSize = 50_000_000_0;
+
+            foreach (int threadCount in threadCounts)
+            {
+                var stopwatch = Stopwatch.StartNew();
+
+                ParallelOptions options = new ParallelOptions
+                {
+                    MaxDegreeOfParallelism = threadCount
+                };
+
+                long total = 0;
+                object locker = new object();
+
+                Parallel.For(0, threadCount, options, i =>
+                {
+                    long localSum = 0;
+
+                    for (int j = 0; j < workSize / threadCount; j++)
+                    {
+                        localSum += j % 3;
+                    }
+
+                    lock (locker)
+                    {
+                        total += localSum;
+                    }
+                });
+
+                stopwatch.Stop();
+
+                Console.WriteLine($"Потоки: {threadCount}, Время: {stopwatch.ElapsedMilliseconds} ms");
+            }
+
+            Console.WriteLine("Готово");
+        }
+    }
+}
